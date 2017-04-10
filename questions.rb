4af6e02e9098c4f4f352df44499991f9040fca44
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-10 23:12:15 +0000

    Create questions class

--- questions.rb
+++ questions.rb
@@ -1,5 +1,20 @@
 class Questions
-  def self.generate_question
-
+  def initialize
+    @num1 = rand(20)
+    @num2 = rand(20)
   end
+  
+  def generate_question
+    puts "What does #{@num1} plus #{@num2} equl?"
+  end
+
+  def check_answer
+    answer = gets.chomp.to_i
+    is_answer_correct?(answer)
+  end
+
+  def is_answer_correct?(answer)
+    answer == @num1 + @num2
+  end
+  
 end
